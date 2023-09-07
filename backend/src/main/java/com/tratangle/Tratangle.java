3e0472d21739b33ccebaa67a723ddb56f1dc90e5
Dylan Oh <dylanoh_1128@outlook.com>
2023-09-07 23:42:43 +0800

    Added S3 config

---
+++ backend/src/main/java/com/tratangle/Tratangle.java
@@ -0,0 +1,13 @@
+package com.tratangle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tratangle {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tratangle.class, args);
+    }
+
+}
---
+++ backend/src/main/java/com/tratangle/config/S3Config.java
@@ -0,0 +1,31 @@
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.regions.Regions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.accessKeyId}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+}
